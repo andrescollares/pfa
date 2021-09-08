71e4eba8a6dd46e9f2ff227248045fbae6aaf2ae
Agustin Bologna <agustin.bologna@gmail.com>
2021-09-08 17:50:49 -0300

    ejercicio 5 parte b

--- obligatorio1/ej5.hs
+++ obligatorio1/ej5.hs
@@ -1,13 +1,33 @@
-import Test.QuickCheck
+import Test.QuickCheck 
 
 -- a)
 esPalindromo :: (Eq a) => [a] -> Bool
 esPalindromo l = l == reverse l
 
+esPalindromo' :: (Eq a) => Palindrome a -> Bool
+esPalindromo' (P l) = l == reverse l
+
 -- b)
+data Palindrome a = P [a]
+    deriving (Eq , Show)
+
+instance Arbitrary a => Arbitrary (Palindrome a) where
+    arbitrary = sized arbitraryPalindrome
+
+unwrap :: Palindrome a -> [a]
+unwrap (P x) = x
+
 -- impone alguna restricción, es decír, no chequear con units supongo.
-
-
+arbitraryPalindrome :: Arbitrary a => Int -> Gen (Palindrome a)
+arbitraryPalindrome 0 = return (P [])
+arbitraryPalindrome n =
+  frequency
+    [ (1, return (P [])),
+      (4, do x <- arbitrary 
+             list <- arbitraryPalindrome (n - 1)
+             return (P (x ++ unwrap list ++ reverse x))
+      )
+    ]
 
 -- c)
 

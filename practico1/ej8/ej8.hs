a6c8e181b10fe2cb37ab79273c7b582cf2d1d7fd
Andrés Collares <andres.collares@fing.edu.uy>
2021-09-10 21:34:12 -0300

    ej8algo

--- practico1/ej8/ej8.hs
+++ practico1/ej8/ej8.hs
@@ -4,27 +4,27 @@
 n = 1000000
 
 -- (a)
-main = print $ f [1 .. n]
+-- main = print $ f [1 .. n]
 
 -- i.
 -- f = foldl (flip (:)) [ ]
 -- i'.
 -- f = foldl' (flip (:)) []
 -- ii.
-f = foldr (\x r -> r ++ [x]) []
+-- f = foldr (\x r -> r ++ [x]) []
  
 -- (b)
--- main = print $ f [1 .. n] [1 .. n]
+main = print $ f [1 .. n] [1 .. n]
 
 -- i.
 -- f = \xs ys -> foldr (:) ys xs
 -- ii.
--- f = foldl (\k x -> k . (x :)) id
+f = foldl (\k x -> k . (x :)) id
 
 -- (c)
 -- main = print f
 
 -- i.
--- f = let xs = [1 .. n ] in if length xs > 0 then head xs else 0
+-- f = let xs = [1..n] in if length xs > 0 then head xs else 0
 -- ii.
--- f = if length [1 .. n] > 0 then head [1 .. n] else 0
+-- f = if length [1..n] > 0 then head [1 .. n] else 0

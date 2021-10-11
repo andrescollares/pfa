86bceb2e06d7602ec3032f6144566d59524af54d
Agustín Bologna <agustin.bologna@gmail.com>
2021-10-11 17:27:05 -0300

    Ej1

---
+++ practico3/ej1.hs
@@ -0,0 +1,37 @@
+import Control.Monad.State
+
+f = do
+  x <- g1
+  y <- g2
+  return x
+
+h1 = do
+  x <- f
+  z <- g3
+  return (x, z)
+
+h2 = do
+  x <- g1
+  y <- g2
+  z <- g3
+  return (x, z)
+
+h3 = do
+  x <- g1
+  z <- g3
+  return (x, z)
+
+g1 = do
+  state <- put 1
+  return get state
+
+g2 :: State Int ()
+g2 = modify (+ 1)
+
+g3 :: State Int Int
+g3 = get
+
+main = do
+  print $ evalState h1 1
+  print $ evalState h2 1
+  print $ evalState h3 1

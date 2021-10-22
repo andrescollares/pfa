a35d4f7faa3f31b3f2ed59585ac9b252a03141b0
Agustín Bologna <agustin.bologna@gmail.com>
2021-10-22 17:43:25 -0300

    Update ej4.hs
    Ej 4 terminado

--- practico3/ej4.hs
+++ practico3/ej4.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 -- import Control.Monad
-import Control.Monad.Catch.Pure (Handler (Handler))
+-- import Control.Monad.Catch.Pure (Handler (Handler))
 import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.Trans.Reader (ReaderT (ReaderT))
@@ -62,14 +64,14 @@ evalE e = join $ runExceptT $ runReaderT (runIdentityT (interp e)) []
 
 -- c)
 
-data Res a = DivZero 
+data Res a = DivZero
            | Unbound String
            | Res a
 
 newtype ResT m a = ResT {runResT :: m (Res a)}
 
 instance Monad m => Functor (ResT m) where
-    fmap f (ResT g) = ResT $ do {resa <- g; return $ fres f resa} 
+    fmap f (ResT g) = ResT $ do {resa <- g; return $ fres f resa}
         where fres f (Res a) = Res $ f a
               fres _ DivZero = DivZero
               fres _ (Unbound s) = Unbound s
@@ -80,6 +82,8 @@ instance Monad m => Applicative (ResT m) where
         where fres (Res f) (Res a) = Res $ f a
               fres _ DivZero = DivZero
               fres _ (Unbound s) = Unbound s
+              fres DivZero _ = DivZero
+              fres (Unbound s) _ = Unbound s
 
 instance Monad m => Monad (ResT m) where
     return a = ResT $ return (Res a)
@@ -87,6 +91,31 @@ instance Monad m => Monad (ResT m) where
         where fres f (Res a) = f a
               fres _ DivZero = ResT $ return DivZero
               fres _ (Unbound s) = ResT $ return $ Unbound s
+
+instance Functor Res where
+    fmap f (Res a) = Res (f a)
+    fmap f (Unbound s) = Unbound s
+    fmap f DivZero = DivZero
+
+instance Applicative Res where
+    pure a = Res a
+    (Res g) <*> (Res k) = Res (g k)
+    _ <*> DivZero = DivZero
+    _ <*> (Unbound s) = Unbound s
+    DivZero <*> _ = DivZero
+    (Unbound s)  <*> _ = Unbound s
+
+instance Monad Res where
+    return a = Res a
+    m >>= f  = do {resa <- m; f resa}
+
+instance MonadError Error (ResT Res) where
+  throwError (ErrorUnbound s) = ResT (Unbound s) 
+  throwError ErrorDivZero = ResT DivZero 
+  
+  catchError (ResT (Unbound s)) _ = ResT (Unbound s)
+  catchError (ResT DivZero) _ = ResT DivZero
+  catchError (ResT (Res r)) _ = ResT (Res r)
 
 evalR :: Expr -> Res Int
 evalR e = join $ runResT $ runReaderT (runIdentityT (interp e)) []
